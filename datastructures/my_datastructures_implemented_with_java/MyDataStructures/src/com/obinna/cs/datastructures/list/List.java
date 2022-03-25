1832a749a3e994bbd0b6fa8550c1b9cdd30700e8
Prof OKalu <okalu1@outlook.com>
2022-03-24 23:19:40 -0500

    Added list of methods TODO in the List ADT (List interface)

--- datastructures/my_datastructures_implemented_with_java/MyDataStructures/src/com/obinna/cs/datastructures/list/List.java
+++ datastructures/my_datastructures_implemented_with_java/MyDataStructures/src/com/obinna/cs/datastructures/list/List.java
@@ -11,5 +11,23 @@ public interface List<E> {
     public abstract void add(E e);
     public void reverse();
     public abstract String toString();
+    /* TODO
+    * addFirst(E e)
+    * addLast(E e)
+    * insertAt(int index, E e)
+    * get(int index)
+    * findFirst(E e)
+    * findLast(E e)
+    * findAny(E e)
+    * findAll(E e)
+    * getIndexOf(E e)
+    * removeFirst()
+    * removeLast()
+    * remove(E e)
+    * removeAll(E e)
+    * removeAt(int index)
+    * clear()
+    * contains(E e): boolean
+    */
 
 }
